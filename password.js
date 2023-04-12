24bed53961fb85a13ac832d8c400810566ddeeac
dynamic dude <105033232+Samuelndegwakiarie@users.noreply.github.com>
2023-04-12 23:16:18 +0300

    Create password.js

---
+++ password.js
@@ -0,0 +1,11 @@
+function generatePassword(length) {
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+  var password = "";
+  for (var i = 0, n = charset.length; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
+
+// Example usage
+console.log(generatePassword(12)); // Generates a random password with 12 characters
